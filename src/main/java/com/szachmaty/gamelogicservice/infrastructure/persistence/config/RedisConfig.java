a33145208626abdd701756b077288f17de82d422
bilskik <kbilski77@gmail.com>
2024-03-11 18:36:16 +0100

    feat: Setup environment(Dockerfile, properties)

--- src/main/java/com/szachmaty/gamelogicservice/infrastructure/persistence/config/RedisConfig.java
+++ src/main/java/com/szachmaty/gamelogicservice/infrastructure/persistence/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.szachmaty.gamelogicservice.infrastructure.persistence.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -13,7 +14,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableRedisRepositories
+@Profile("dev")
 public class RedisConfig {
+
     @Bean
     public JedisConnectionFactory connectionFactory() {
         var config = new RedisStandaloneConfiguration();
